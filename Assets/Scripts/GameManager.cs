3e7aab5c47be42750c683eb9b84ccab7cfc399ec
Joshua Salazar <afterlifejosh@yahoo.com>
2018-08-14 20:17:40 -0700

    Commiting current project beginnings.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class GameManager : MonoBehaviour 
+{
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Paddle : MonoBehaviour 
+{
+    public PaddleSide Side;
+
+    public enum PaddleSide { LEFT, RIGHT };
+
+    private float inputFloat;
+
+    void Update()
+    {
+        if (Side == PaddleSide.LEFT)
+        {
+            inputFloat = Input.GetAxis("LeftPaddle");
+        }
+        else if (Side == PaddleSide.RIGHT)
+        {
+            inputFloat = Input.GetAxis("RightPaddle");
+        }
+
+        transform.Translate(Vector3.up * inputFloat * Time.deltaTime, Camera.main.transform);
+    }
+}
