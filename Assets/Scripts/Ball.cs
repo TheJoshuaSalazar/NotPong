a77a71bd52db70855d78a16ae1cd608b478877ae
Joshua Salazar <afterlifejosh@yahoo.com>
2018-08-15 20:58:55 -0700

    Removed the ignore .meta. Added meta files. Ball moves and bounces off of object.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// </summary>
+public class Ball : MonoBehaviour
+{
+    public float Force;
+    public Rigidbody rb;
+
+    private Vector2 vel;
+
+    private void Start()
+    {
+        rb.AddForce(new Vector3(1, 1, 0) * Force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.attachedRigidbody != null)
+        {
+            vel.x = rb.velocity.x;
+            vel.y = rb.velocity.y;
+            Debug.Log(rb.velocity);
+            rb.velocity = -vel;
+            Debug.Log(rb.velocity);
+            //rb.AddForce(-vel);
+        }
+    }
+}
--- Assets/Shaders/PaddleShader.shader
+++ Assets/Shaders/PaddleShader.shader
@@ -1,11 +1,15 @@
-﻿Shader "Custom/PaddleShader" {
-	Properties {
+﻿Shader "Custom/PaddleShader" 
+{
+	Properties 
+	{
 		_Color ("Color", Color) = (1,1,1,1)
 		_MainTex ("Albedo (RGB)", 2D) = "white" {}
 		_Glossiness ("Smoothness", Range(0,1)) = 0.5
 		_Metallic ("Metallic", Range(0,1)) = 0.0
 	}
-	SubShader {
+
+	SubShader 
+		{
 		Tags { "RenderType"="Opaque" }
 		LOD 200
 
